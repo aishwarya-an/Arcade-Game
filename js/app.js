6a2e35344e78cb1f5b63582c47500ec37feb5f9a
Aishwarya A N <aishwarya.an95@gmail.com>
2017-02-04 22:08:37 +0530

    Added the Player functionality to app.js.
    * The player can move up, down, left and right within the boundaries.
    * On reaching the river, the game is reset.
    * Collisions are not handled.
    
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// Constants
+var PLAYER_BOTTOM_BOUNDARY = 405;
+var PLAYER_RIGHT_BOUNDARY = 500;
+var PLAYER_X = 200;
+
 // Enemies our player must avoid
 // Parameter: y, the y-co-ordinate of the enemy
 // Parameter: speed, the speed of enemy moving
@@ -29,17 +34,66 @@ Enemy.prototype.render = function() {
 };
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class 
+var Player = function() {
+  this.x = PLAYER_X;
+  this.y = PLAYER_BOTTOM_BOUNDARY;
+  this.sprite = 'images/char-cat-girl.png';
+};
 
+// Update the player's position if the player has moved out of boundary
+Player.prototype.update = function() {
+  // If player has moved out of the left boundary
+  if(this.x < 0)
+    this.x = 0;
+  // If the player has moved out of the right boundary
+  else if(this.x > PLAYER_RIGHT_BOUNDARY)
+    this.x = PLAYER_RIGHT_BOUNDARY;
+  // If the player has reached the river, then reset the game after 500ms
+  if(this.y <= 0){
+    // p is used in the setTimeout callback instead of 'this' because the
+    // 'this' in setTimeout callback does not refer to the Player instance
+    // Hence, 'this' is stored in p.
+    var p = this;
+    setTimeout(function(){
+        p.reset();
+        }, 500);
+  }
+  else if(this.y > PLAYER_BOTTOM_BOUNDARY)
+    this.y = PLAYER_BOTTOM_BOUNDARY;
+};
 
-// Place the player object in a variable called player
+// Draw the player on the screen
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Moves the player in case of key presses.
+// Called by the document's EventListener() for key presses.
+// Parameter: input, a string containing the value of keypress
+Player.prototype.handleInput = function(input) {
+  if(input == 'left')
+    this.x -= 100;
+  if(input == 'up' && this.y - 70 >= 0)
+    this.y -= 83;
+  if(input == 'right' && this.x + 100 < PLAYER_RIGHT_BOUNDARY)
+    this.x += 100;
+  if(input == 'down')
+    this.y += 83;
+};
+
+// Resets the game, either in case of player reaching the river or 
+// in case of player colliding with enemy.
+Player.prototype.reset = function(){
+  this.x = PLAYER_X;
+  this.y = PLAYER_BOTTOM_BOUNDARY;
+};
 
 // Placing all enemies in an array.
 var allEnemies = [new Enemy(60, 100), new Enemy(145, 50), new Enemy(225, 80), 
     new Enemy(60, 25), new Enemy(145, 150)];
 
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -50,6 +104,6 @@ document.addEventListener('keyup', function(e) {
     39: 'right',
     40: 'down'
 };
-
-// player.handleInput(allowedKeys[e.keyCode]);
+  
+  player.handleInput(allowedKeys[e.keyCode]);
 });
